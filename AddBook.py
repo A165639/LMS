30491b40ae479ca7e6abbf5e33d101dd0bd8ec2e
A165639 <91881169+A165639@users.noreply.github.com>
2024-02-12 12:15:50 +0530

    Update AddBook.py

--- AddBook.py
+++ AddBook.py
@@ -38,7 +38,7 @@ def addBook():
 
     # Add your own database name and password here to reflect in the code
     mypass = "root"
-    mydatabase="db"ll
+    mydatabase="db"
 
     con = pymysql.connect(host="localhost",user="root",password="1234@",database=mydatabase)
     cur = con.cursor()
